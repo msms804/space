a8e54a0ace2469a44454e953dbc34a1a79a3f3c1
msms804 <104570214+msms804@users.noreply.github.com>
2024-02-20 23:49:11 +0900

    폴더정리

---
+++ src/Component/chatList.jsx
@@ -0,0 +1,8 @@
+//채팅들 보여주는 컴포넌트
+import React from "react";
+const ChatList = () => {
+    return (<>
+        챗리스트
+    </>)
+}
+export default ChatList;
---
+++ src/Component/chatbox.jsx
@@ -0,0 +1,8 @@
+//채팅작성하는 컴포넌트
+import React from "react";
+const ChatBox = () => {
+    return (<>
+        챗박스
+    </>)
+}
+export default ChatBox;
